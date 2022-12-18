40355e3131f21a3b6f790452a4febdb43f232d12
sist-varm <sist.varm@gmail.com>
2022-12-18 15:47:06 +0900

    first commit

---
+++ src/test/java/com/varm/zoe/ZoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.varm.zoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
